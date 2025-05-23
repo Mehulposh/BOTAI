bbcb34da227b23549e40fe88e591badc7bde8c50
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-05-23 11:34:05 +0530

    added a card for past chats

---
+++ src/components/HistoryCard/HistoryCard.jsx
@@ -0,0 +1,47 @@
+import { Stack,Typography } from "@mui/material";
+import { format,isEqual,startOfDay,add } from "date-fns";
+import ConversationCard from "../ConversationCard/ConverationCard";
+
+
+function historycard({details}){
+    
+    const CustomDate = (date) => {
+        const today = startOfDay(new Date());
+
+        if(isEqual(today,date)){
+            return "Today's Chats";
+        }
+        else if(isEqual(today,add(date,{days: 1}))){
+            return "Yesterdays' Chats";
+        }
+        else{
+            return format(date,'do LLL yyyy');
+        }
+    };
+
+    
+    return (
+        <Stack
+            spacing={2}
+        >
+            <Typography
+                fontWeight={700}
+            >
+                {CustomDate(startOfDay(new Date(details.datetime)))}
+            </Typography>
+
+            <Stack
+                spacing={2}
+            >
+                {details.chat.map((item) => {
+                    return <ConversationCard details={item} readOnly={true} key={item.id}/> ;
+                })}
+            </Stack>
+        </Stack>
+    );
+
+}
+
+
+
+export default historycard;
